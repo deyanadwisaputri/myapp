d2528f50a28c65ecc678e493f527c19c3d580f8c
deyanasaputri <deyanasaputri@gmail.com>
2025-05-05 03:17:44 +0000

    first commit

---
+++ lib/login_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool statusPassword = true;
+  bool statusWarna = true;
+
+  tampilPassword() {
+    setState(() {
+      statusPassword = !statusPassword;
+    });
+  }
+
+  gantiWarna() {
+    setState(() {
+      statusWarna = !statusWarna;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: statusWarna ? Colors.blue : Colors.red,
+      body: Column(
+        children: [
+          TextField(),
+          TextField(
+            obscureText: statusPassword,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              hintText: 'Enter your password',
+              prefixIcon: Icon(Icons.lock),
+              suffixIcon: IconButton(
+                icon: Icon(
+                  statusPassword ? Icons.visibility_off : Icons.visibility,
+                ),
+                onPressed: () {
+                  tampilPassword();
+                },
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              gantiWarna();
+            },
+            child: Text('Ganti warna'),
+          ),
+        ],
+      ),
+    );
+  }
+}
